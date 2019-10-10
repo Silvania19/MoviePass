b69223d031964b46b1e69b7b37d1acdac6f7c5e6
Silvania19 <silvaniaortega17@gmail.com>
2019-10-10 16:05:58 -0300

    Update autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -1,1 +1,15 @@
 <?php
+namespace config;
+include ("config/constantes.php");
+
+class Autoload{
+    public static function Start()
+    {
+        spl_autoload_register(function ($class)
+        {
+            $class2=ROOT."/". strtolower(str_replace("\\", "/",$class).'.php');
+            include_once ($class2);
+
+        });
+    }
+}
