6a59179ef09e511d1d04d2d13cdbc950170b3a3a
Silvania19 <silvaniaortega17@gmail.com>
2019-10-10 16:16:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+    <?php
+    include ('header.php');
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+        include ('config/autoload.php');
+        use config\Autoload as auto;
+        use model\Person as per;
+        auto::Start();
+        $persona=new per('silvania');
+        
+        echo $persona->getName();
+        include ('footer.php');
+    ?>
