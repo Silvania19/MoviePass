a3334024975ce9c8975a8dfb0f31eb36dc27a0b2
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-10-17 13:55:13 -0300

    Update router.php
    routttttttter

--- config/router.php
+++ config/router.php
@@ -1,1 +1,30 @@
 <?php
+namespace config;
+class Router
+{
+    public function construct()
+    {
+
+    }
+    public static function address(Request $request)
+    {
+        $controller= $request->getController()."controller";
+        $method= $request->getMethod();
+        $parameters= $request->getParameters();
+        
+        $class="controllers\\".$controller;//crea la direccion para incluir la clase. el namespace+el nombre de la clase
+
+        $instance= new $class();// crea instancia de la clase
+
+        if(!isset($parameters))
+        {
+            call_user_func(array($instance,$method));
+      }else{
+          call_user_func_array(array($instance,$method),$parameters);
+      }
+
+
+
+    }
+}
+?>
