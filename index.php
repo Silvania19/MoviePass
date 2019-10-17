810c36a53194808d7526bac0350ddcedcd9b79ec
Silvania19 <silvaniaortega17@gmail.com>
2019-10-17 14:08:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
     <?php
     include ('header.php');
+   
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -8,9 +9,14 @@
         include ('config/autoload.php');
         use config\Autoload as auto;
         use model\Person as per;
-        auto::Start();
-        $persona=new per('silvania');
-        
-        echo $persona->getName();
+        auto::Start();?>
+        <p>hola</p>
+        <h1>jgfdsfghjk</h1>
+        <form action="">
+            <input type="email" name="" id="">
+        </form>
+        <?php
+
         include ('footer.php');
     ?>
+
