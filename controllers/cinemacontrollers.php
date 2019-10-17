50e1d2041ed5dc5370ebb33a2eab90ea46240119
Silvania19 <silvaniaortega17@gmail.com>
2019-10-17 14:07:39 -0300

    Update cinemacontrollers.php

---
+++ controllers/cinemacontrollers.php
@@ -0,0 +1,15 @@
+<?php
+namespace controllers;
+use model\cinema as cine;
+class CinemaControllers
+{
+    private $cinemaRepo;//esta variable, sera aquella aque le iguale lo que traiga de la base de datos. Cuando la tenga
+    public function __construct()
+    {
+        $this->cinemaRepo=array();// sera reemplazado por una instancia de la clases donde tenga  la base correspondiente
+    }
+    public function add($cine)
+    {
+         array_push($this->cinemaRepo, $cine);
+    }
+}
