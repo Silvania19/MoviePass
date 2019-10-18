e18897ec093582f1a7aa0945e0da99d337689dee
Silvania19 <silvaniaortega17@gmail.com>
2019-10-18 16:22:41 -0300

    Update cinema.php

--- models/cinema.php
+++ models/cinema.php
@@ -2,6 +2,15 @@
 namespace models;
 class Cinema
 {
+    private $idCine;
     private $name;
+    private $administratorUser;
     
+
+    public function __construct($name, $idCine)
+    {
+        $this->name=$name;
+        $this->idCine=$idCine;
+    }
+
 }
