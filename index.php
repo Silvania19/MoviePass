1f9841500e0c36729dfceda70635f403f9a426a3
Silvania19 <silvaniaortega17@gmail.com>
2019-10-18 16:22:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 
     <?php
-    include ('header.php');
+    
    
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
         include ('config/autoload.php');
+        include ('config/constantes.php');
+        include ('config/request.php');
+        include ('config/router.php');
+       
         use config\Autoload as auto;
-        use model\Person as per;
-        auto::Start();?>
-        <p>hola</p>
-        <h1>jgfdsfghjk</h1>
-        <form action="">
-            <input type="email" name="" id="">
-        </form>
-        <?php
-
-        include ('footer.php');
+        use config\Router as router;
+        use config\Request as request;
+        auto::Start();
+        session_start();
+        router:: address(new request);
     ?>
 
