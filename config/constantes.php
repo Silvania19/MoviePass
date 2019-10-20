8ab4429c337628fa51c3737c3cfa7d951dd2b7c5
Silvania19 <silvaniaortega17@gmail.com>
2019-10-20 15:39:36 -0300

    Update constantes.php

--- config/constantes.php
+++ config/constantes.php
@@ -1,12 +1,13 @@
 <?php
  
  define ('ROOT', dirname(__DIR__));
-$base = explode($_SERVER["DOCUMENT_ROOT"], ROOT);
- define ("BASE", $base[0]);
+ //$base = explode($_SERVER["DOCUMENT_ROOT"], ROOT);
+ //define ("BASE", $base[0]);
  define ("VIEWS_PATH", "views/"); //esta constante define el camino a las vistas, es decir donde estan guardadas las vistas
  define ("FRONT_ROOT", "/");//DEFINIR BIEN DESPUES
  define ("DB_HOST", "localhost");
  define ("DB_NAME", "MoviePass");
+ define ("CSS", "front/styles/");//la ruta donde se encuentra los archivos css
  
- var_dump($base);
+ 
  ?>
