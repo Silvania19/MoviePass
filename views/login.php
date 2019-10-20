3f763c7f7d0e5dabd7b6f45311ada4352cc0910c
Silvania19 <silvaniaortega17@gmail.com>
2019-10-20 15:42:21 -0300

    Update login.php

--- views/login.php
+++ views/login.php
@@ -1,8 +1,12 @@
-<?php include(VIEWS_PATH."header.php")?>
+<?php include(VIEWS_PATH."header.php");?>
 
-<h5>Iniciar Sesion</h5>
 <body>
-<form action="<?php echo BASE;?>user/login" method ="POST">
+<link rel="stylesheet" href="front/styles/style2.css"><!
+<div class="container borde2">
+<h5>Iniciar Sesion</h5>
+
+
+<form action="" method ="" >
 
 <input type="text" name="name" id=""> UserName
 <input type="password" name="" id=""> Password
@@ -22,5 +26,5 @@
 <input type="password" name="" id=""> Password
 <button type="submit">sign up</button>
 
-</form>-->
+</form>--></div>
 <?php include(VIEWS_PATH."footer.php")?>
