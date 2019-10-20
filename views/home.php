aee1c3987b5c2f22edb3a757bafeecda31a475af
Silvania19 <silvaniaortega17@gmail.com>
2019-10-20 15:42:17 -0300

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,9 @@
+<?php
+include(VIEWS_PATH."header.php");
+
+?>
+ <link rel="stylesheet" href="front/styles/style2.css">
+<div class="container">
+<form action="">
+    <input type="" class="tex"> tuculo
+</form></div>
