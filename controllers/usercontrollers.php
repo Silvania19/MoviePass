75f8ea3285d8061e47fa5c8729047a9ab762ccf8
Silvania19 <silvaniaortega17@gmail.com>
2019-10-21 16:20:58 -0300

    Update usercontrollers.php

--- controllers/usercontrollers.php
+++ controllers/usercontrollers.php
@@ -1,40 +1,49 @@
 <?php
 namespace controllers;
 use models\User as User;
-use daos\userdao as userD;
+use daosjson\UserDao as userD;
 class UserControllers
 {
 private $daoUser;//en esta variable tiene una instancia de la clase Dao user, 
 public function __construct()
 {
-    $daoUser = new userD();
+    $this->daoUser = new userD();
 }
-   public function login($email, $password)
-   {
-    $listUser=$daoUser->GetAll();
-    foreach ($listUser as $value) {
-        foreach ($value as $key as $value2)
+public function login()
+{  
+    $email=$_POST['email'];
+    $password=$_POST["password"];
+ 
+    $user=$this->daoUser->Search($email);
+    if(isset($user))
+    {
+        if($user->getPassword()==$password)
         {
-
-            if ($value2==$email)
-            {
-                if($value2==$password)
-                {
-                  $user = $value;
-                session_start($user);
-                }
-                else 
-                {
-                    echo ' incorrect password';
-                }
-               
-            }
-            else{
-                echo 'incorrect user';
-            }
+            $_SESSION[$user];
+            echo "ya esta perra";
         }
-      
-    }
+        else
+        {
+            echo "incorrect password";
+        }
+        
+    }else{
+            echo"incorect user";
+        }
+    
+}
+public function signUp()
+{
+    $name=$_POST['name'];
+    $lastName=$_POST['lastName'];
+    $dni=$_POST["dni"];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $userName=$_POST['userName'];
+   
+    $user=new User($name, $lastName, $dni, $email,$password, $userName);
+    $this->daoUser->Add($user);
 
-   }
+   var_dump($this->daoUser);
+}
 }
