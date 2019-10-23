ec45327e7b9d2c926f5c78ef8523a40f0cb7dea7
Silvania19 <silvaniaortega17@gmail.com>
2019-10-23 01:15:15 -0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,9 +1,29 @@
-<?php
-include(VIEWS_PATH."header.php");
-
+<?php include(VIEWS_PATH."header.php");
 ?>
- <link rel="stylesheet" href="front/styles/style2.css">
-<div class="container">
-<form action="">
-    <input type="" class="tex"> tuculo
-</form></div>
+
+ 
+
+<main class="login">
+<link rel="stylesheet" href="front/styles/style2.css">
+<div class="container-fluid">
+        <div class="row">
+            <div class="col">
+           <header class="text-center">
+<h1>   <p class="bg-primary display-1 ">MOVIE PASS</p> </h1>
+</header>   
+  <img src="front/img/dog.jpeg" class=" float-right rounded-circle"> 
+  </div>
+    </div>
+</div>
+    
+<?php  include(VIEWS_PATH."login.php");
+ use controllers\MovieControllers as movieC;
+ $list=new movieC();
+ $list->seeListMovie
+ ?>
+</main>
+<?php include(VIEWS_PATH."footer.php");?>
+
+
+
+
