73ab0b3c531362feda483cb805318eb0e0d863db
Silvania19 <silvaniaortega17@gmail.com>
2019-10-23 01:14:46 -0300

    Update Idaos.php

--- interfaces/Idaos.php
+++ interfaces/Idaos.php
@@ -1,10 +1,10 @@
 <?php
 namespace interfaces;
-interface iDaos
+interface Idaos
 {
     public function GetAll();
     public function Add($objeto);
-    public function Delete($objeto);
+    public function Delete($email);
     public function Update($objeto);
     public function Search($email);
 }
