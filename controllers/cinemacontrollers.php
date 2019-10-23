734ed7f28baadb5cee9f3a31d01f3e05b5d55abb
Silvania19 <silvaniaortega17@gmail.com>
2019-10-23 01:13:33 -0300

    Update cinemacontrollers.php

--- controllers/cinemacontrollers.php
+++ controllers/cinemacontrollers.php
@@ -1,15 +1,14 @@
 <?php
 namespace controllers;
-use models\cinema as cine;
-class CinemaControllers
+use models\Cine as cine;
+use daosjson\CineDaos as cineD;
+class CineControllers
 {
     private $cinemaRepo;//esta variable, sera aquella aque le iguale lo que traiga de la base de datos. Cuando la tenga
     public function __construct()
     {
-        $this->cinemaRepo=array();// sera reemplazado por una instancia de la clases donde tenga  la base correspondiente
+        $this->cinemaRepo= new cineD();// sera reemplazado por una instancia de la clases donde tenga  la base correspondiente
     }
-    public function add($cine)
-    {
-         array_push($this->cinemaRepo, $cine);
-    }
+    public function
+   
 }
