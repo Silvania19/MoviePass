a230fc9cdf46d85eef3426b1232b0da470f85f76
Silvania19 <silvaniaortega17@gmail.com>
2019-10-23 01:15:19 -0300

    Create home2.php

---
+++ views/home2.php
@@ -0,0 +1,27 @@
+<?php  include(VIEWS_PATH."header.php");?>
+
+<link rel="stylesheet" href="front/styles/style2.css">
+   <nav class="navbar navbar-expand-sm bg-danger">
+      <ul class="navbar-nav">
+      <a href=""class="navbar-brand"><img src="front/img/dog.jpeg" style="width: 70px;"></a>
+ 
+    <li class="nav-item dropdown">
+      <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">MOVIES</a>
+        <div class="dropdown-menu">
+          <a href=""class="dropdown-item">Estrenos</a>
+          <a href=""class="dropdown-item">Destacadas</a>
+          <a href=""class="dropdown-item">ñe</a>
+        </div> 
+   </li>
+    <li class="nav-item dropdown">    
+        <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">USER</a>
+        <div class="dropdown-menu">
+          <a href=""class="dropdown-item">Mi perfil</a>
+          <a href=""class="dropdown-item">Modificar</a>
+          <a href=""class="dropdown-item">Eliminar cuenta</a>
+         
+        </div>
+   </li>
+    </nav>
+  
+    <?php include(VIEWS_PATH."footer.php");?>
