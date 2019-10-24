74d8ab92726b39abdb7c7252f88b32b1562999b1
Silvania19 <silvaniaortega17@gmail.com>
2019-10-24 17:47:55 -0300

    Update moviecontrollers.php

--- controllers/moviecontrollers.php
+++ controllers/moviecontrollers.php
@@ -1,19 +1,23 @@
 <?php
 namespace controllers;
 use models\Movie as Movie;
-use daosjson\movieDao as movieD;
+use daosjson\MovieDao as movieD;
+use daosjson\GenresDao as genresD;
 class MovieControllers
 { 
     private $listMovie;
+    private $listGenres;
     
     public function __construct()
     {
         $this->listMovie=new movieD();
+        $this->listGenres=new genresD();
     }
 
     public function seeListMovie()
     {
-        $listMovie2=$this->listMovie->getNowMovie();
+           $listMovie2=$this->listMovie->getNowMovie();
+         $listGenres2=$this->listGenres->GetAll();
            include(VIEWS_PATH."movieviews.php");
     } 
 }
