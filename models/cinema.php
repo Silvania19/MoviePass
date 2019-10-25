4a2ee1b0cd334f71b04e8cea62c24976be1fa913
Silvania19 <silvaniaortega17@gmail.com>
2019-10-25 14:24:43 -0300

    Update cinema.php

--- models/cinema.php
+++ models/cinema.php
@@ -2,6 +2,48 @@
 namespace models;
 
 class Cinema{
-   private idSala;
-   private 
+   private $idCinema;
+   private $idCine;
+   private $numberCinema;
+   private $capacity;
+
+   public function __construct($idCinema, $idCine, $numberCinema, $capacity)
+   {
+      $this->idCinema=$idCinema;
+      $this->idCine=$idCine;
+      $this->numberCinema=$numberCinema;
+      $this->capacity=$capacity;
+   }
+   public function setIdCinema($idCinema)
+   {
+      $this->idCinema=$idCinema;
+   }
+   public function setIdCine($idCine)
+   {
+      $this->idCine=$idCine;
+   }
+   public function setNumberCinema($numberCinema)
+   {
+      $this->numberCinema=$numberCinema;
+   }
+   public function setCapacity($capacity)
+   {
+      $this->capacity=$capacity;
+   }
+   public function getIdCinema()
+   {
+      return $this->idCinema;
+   }
+   public function getIdCine()
+   {
+      return $this->idCine;
+   }
+   public function getNumberCinema()
+   {
+      return $this->numberCinema;
+   }
+   public function getCapacity()
+   {
+      return $this->capacity;
+   }
 }
