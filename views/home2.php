5069f8368d509813eea32a48e9c47f2cc25ea056
Silvania19 <silvaniaortega17@gmail.com>
2019-10-25 14:24:38 -0300

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -1,20 +1,20 @@
 <?php  include(VIEWS_PATH."header.php");?>
-
-<link rel="stylesheet" href="front/styles/style2.css">
    <nav class="navbar navbar-expand-sm bg-danger">
       <ul class="navbar-nav">
-      <a href=""class="navbar-brand"><img src="front/img/dog.jpeg" style="width: 70px;"></a>
+        <a href=""class="navbar-brand"><img src="front/img/dog.jpeg" style="width: 70px;"></a>
  
-    <li class="nav-item dropdown">
-      <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">MOVIES</a>
-
-   </li>
-    <li class="nav-item dropdown">    
-        <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">USER</a>
-        <div class="dropdown-menu">
-          <a href=""class="dropdown-item">Mi perfil</a>
-          <a href=""class="dropdown-item">Modificar</a>
-          <a href=""class="dropdown-item">Eliminar cuenta</a>
+            <ul class="nav-item dropdown">
+              
+                <li class ="nav-item">
+                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">MOVIES</a>
+                </li>
+                <li class ="nav-item">
+                  <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">CINES</a>
+                </li>
+                <li class ="nav-item">
+                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">USER</a>
+                </li>
+            </ul>
          
         </div>
     </li>
