4141b3ff125a2ec88e8a5cdd88b964877a8d5c77
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-10-28 14:31:12 -0300

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -3,23 +3,24 @@
 <link rel="stylesheet" href="front/styles/style2.css">
    <nav class="navbar navbar-expand-sm bg-danger">
       <ul class="navbar-nav">
-        <a href=""class="navbar-brand siForm"><img src="front/img/dog.jpeg" style="width: 70px;"></a>
+        <a href=""class="navbar-brand siForm"><img src="/MoviePass/front/img/dog.jpeg" style="width: 70px;"></a>
  
-            <ul class="nav-item dropdown">
+            <ul class="nav ">
               
                 <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">MOVIES</a>
+                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link " >MOVIES</a>
                 </li>
                 <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">CINES</a>
+                  <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link " >CINES</a>
                 </li>
                 <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navbardrop">USER</a>
+                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link">USER</a>
                 </li>
             </ul>
          
         </div>
     </li>
     </nav>
+    
   
     <?php include(VIEWS_PATH."footer.php");?>
