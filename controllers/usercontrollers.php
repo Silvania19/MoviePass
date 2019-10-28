435ebc44818ab93190c262890f47472fd5c54aa1
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-10-28 17:35:05 -0300

    Update usercontrollers.php

--- controllers/usercontrollers.php
+++ controllers/usercontrollers.php
@@ -41,9 +41,28 @@ public function signUp()
     $dni=$_POST["dni"];
     $email=$_POST['email'];
     $password=$_POST['password'];
-    
     $user=new User($name,$lastName, $dni, $email, $password);
     $this->daoUser->Add($user);
-    var_dump($this->daoUser);
+    $_SESSION['user']=$user;//pongo en session al nuevo usuario qye se acabo de resistrar
+    include(VIEWS_PATH."home2.php");
+}
+public function deleteUser()
+{
+     
+     if(isset($_SESSION['user']))
+     {
+
+       $user=$_SESSION['user'];
+     } 
+     if (isset($user))
+     {
+         $email=$user->getEmail();
+         $this->daoUser->Delete($email);
+         include("index.php");
+     }
+}
+public function checkSession()
+{
+    
 }
 }
