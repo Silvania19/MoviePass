2beb40f0522e8368cf39b7101b615df8b6827681
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-10-28 17:34:56 -0300

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
-<script src="/MoviePass/front/js/jquery-3.3.1.slim.min.js"></script>
-<script src="/MoviePass/front/js/popper.min.js"></script>
+<script src="<?php echo FRONT_ROOT;?>/front/js/jquery-3.3.1.slim.min.js"></script>
+<script src="<?php echo FRONT_ROOT;?>/front/js/popper.min.js"></script>
 
-<script src="/MoviePass/front/js/bootstrap.min.js"></script>
+<script src="<?php echo FRONT_ROOT;?>/front/js/bootstrap.min.js"></script>
 
 </body>
 </html>
