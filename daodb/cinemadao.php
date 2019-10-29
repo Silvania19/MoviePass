a305e02134fafc4db0c38384e382e5b6d481beda
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-10-29 16:56:15 -0300

    Create cinemadao.php

---
+++ daodb/cinemadao.php
@@ -0,0 +1,97 @@
+<?php
+namespace daodb;
+use models\Cinema as cinema;
+USE interfaces\Idaos as Idaos;
+use daodb\Connection as   Connection;
+
+class CinemaDao implements Idaos
+{
+    private $connection;
+    public function __construct()
+    {
+
+    }
+    public function GetAll()
+    {
+        
+        $sql="SELECT * FROM cinemas";
+        try {
+            $this->connection = Connection::getInstance();
+            $listCinemas = $this->connection->execute($sql);  
+           
+        } catch (\PDOException  $ex) {
+            throw $ex;
+        }
+        if (!empty($listCinemas))
+        {
+            return $this->mapear($listCinemas);
+          
+        }
+        else{
+            return false;
+        }
+    }
+    public function Add($objeto)
+    {
+        // Guardo como string la consulta sql utilizando como values, marcadores de parámetros con nombre (:name) o signos de interrogación (?)
+        // por los cuales los valores reales serán sustituidos cuando la sentencia sea ejecutada
+        $sql="INSERT INTO cinemas (idCine, numberCinema, capacity) VALUES (:idCine,:numberCinema,capacity)";
+      
+        $parameters["idCine"]=$objeto->getIdCine();
+        $parameters['numberCinema']=$objeto->getNumberCinema();
+        $parameters["capacity"]=$objeto->getCapacity();
+    
+        try {
+            $this->connection= Connection::getInstance();
+            return $this->connection->executeNonQuery($sql, $parameters);
+        } catch (\PDOException $ex) {
+            throw $ex;
+        }
+    }
+   /**
+    * convertivos lo que traemos de la base de datos que es un arreglo donde 
+    *cada posicion lo que tiene es un arreglo con los datos, a un arreglo de objeto del tipo al que pertece.
+   **/
+   private function mapear($arreglo)
+   {
+       $arreglo=is_array($arreglo)?$arreglo:[];
+       $arregloObjetos=array_map(function($pos)
+       {
+        $newCine =new cine($pos['idCine'],$pos['numberCinema'], $pos['capacity']);
+        $newCine->setIdCinema($pos['idCinema']);
+        return $newCinema;
+       }, $arreglo);
+       return count($arregloObjetos)>1? $arregloObjetos: $arregloObjetos['0'];
+   }
+    public function Delete($)
+    {
+
+    }
+    public function Update($objeto, $buscador)
+    {
+      
+    }
+    public function Search($objeto)
+    {
+      $sql="SELECT * FROM cinemas where objeto=:numberCinema";   
+      $parameters['numberCinema']=$objeto;
+      try {
+          $this->connection = Connection:: getInstance();
+          $resul=$this->connection->execute($sql, $parameters);
+      } catch (\PDOException $th) {
+          throw $th;
+      }
+      if(!empty ($resul))
+      {
+          return $this->mapear($resul);
+      }
+      else
+      {
+          return  false;
+      }
+    }
+}
+?>
+
+
+?>
