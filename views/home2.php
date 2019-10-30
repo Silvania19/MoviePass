e2cd2a534401e37c08472378f29a4eb163510f79
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-10-30 13:47:05 -0300

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -1,6 +1,6 @@
 <?php include(VIEWS_PATH."header.php"); ?>
 
-<link rel="stylesheet" href="<?php echo FRONT_ROOT;?>front/styles/style2.css">
+<link rel="stylesheet" href="<?php echo FRONT_ROOT;?>/front/styles/style2.css">
    <nav class="navbar navbar-expand-sm bg-danger">
       <ul class="navbar-nav">
         <a href=""class="navbar-brand siForm"><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
@@ -14,10 +14,7 @@
                   <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link " >CINES</a>
                 </li>
                 <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link">USER</a>
-                </li>
-                <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="nav-link">SALIR</a>
+                  <a href="<?php echo FRONT_ROOT;?>/views/user" class="nav-link">USER</a>
                 </li>
             </ul>
          
