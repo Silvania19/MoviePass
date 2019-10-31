1c100bdf7a6d3f8307fe4499907a673d5caf6999
Silvania19 <silvaniaortega17@gmail.com>
2019-10-31 00:02:19 -0300

    Update autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -7,7 +7,7 @@ class Autoload{
     {
         //recibimos un nombre de una clase y un namespace como parametro ($class)
         spl_autoload_register(function ($class) 
-        {
+        { 
             //arreglamos la ruta que vamos a incluir en, como el nombre de las clases comienzan con mayusculas
             //y los nombres que las contienen estan con minuscula convertimos todo en minuscula
             $class2=ROOT."/". strtolower(str_replace("\\", "/",$class).'.php');//ROOT es una constante que contiene la ruta raiz, esta defenida en config/constantes
