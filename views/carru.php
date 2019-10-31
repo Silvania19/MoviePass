a889474449093760f39c4c75e7bb354b6f3f3ab4
Silvania19 <silvaniaortega17@gmail.com>
2019-10-31 00:09:08 -0300

    Update carru.php

--- views/carru.php
+++ views/carru.php
@@ -1,6 +1,67 @@
  <?php
 include(VIEWS_PATH."header.php");?>
- <div id="demo" class="carousel slide" data-ride="carousel">
+
+
+<div class="container">
+  <div class="row">
+  
+      <div class="col">
+          <div id="carousel-1" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+              <li data-target=#carousel-1 data-slide-to="0" class="active"></li>
+              
+              </ol>
+              
+              <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                           <img src="" alt="" class="img-fluid">
+                                     <div  class="carousel-caption">
+                                              <h3>Tituo</h3>
+                                                  <p>todo lo q sigue</p>
+                                     </div>
+                    </div>
+              </div>
+         </div>
+ <a href="#carousel-1" class="left carousel-control" role="button" data-slide="prev">
+ <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+ </a>
+ <a href="#carousel-1" class="right carousel-control" data-slide="next"role="button" data-slide="prev">
+ <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+ </a>
+
+     </div>
+  
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container-fluid">
+<div class="row">
+<div class="col">
+ 
  <!--INDICADORES-->
   <ul class="carousel-indicators">
     <li data-target="demo" data-slide-tp=0 class="active"></li>
@@ -11,21 +72,13 @@ include(VIEWS_PATH."header.php");?>
   </ul>
 
  <!--IMAGENES-- esto es para cada imagen hacer dentro del foreach-->
-<div class="carousel-inner">
-    <div class="carousel-item">
-        <img src="" alt="" class="img-fluid">
-            <div  class="carouse-caption">
-            <h3>Tituo</h3>
-            <p>todo lo q sigue</p>
-           </div>
-    </div>
-</div>
+
  <!--CONTROLES DE IZ  DER-->
  
- <a href="#demo" class="carousel-control-prev" data-slide="prev"></a><span class="carousel-control-prev-icon"></span>
- <a href="#demo" class="carousel-control-next" data-slide="next"></a><span class="carousel-control-next-icon"></span>
+
+   </div>
+  </div>
+  </div>
  </div>
-
-
 <?php
 include(VIEWS_PATH."footer.php");?>
