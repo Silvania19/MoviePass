7dbad9b8bcc488482c666bf2ccefccc19eb7dc5c
Silvania19 <silvaniaortega17@gmail.com>
2019-11-04 18:09:32 -0300

    Create carteleraviews.php

---
+++ views/carteleraviews.php
@@ -0,0 +1,13 @@
+<?php include(VIEWS_PATH."header.php");
+?>
+
+gallegos addFuncion <br>
+funciones: <br>
+funcion1. <br>
+funcion2 <br>
+aldrey  addFuncion <br>
+funcion  <br>
+fyuncion1 <br>
+
+
+<?php include(VIEWS_PATH."footer.php");?>
