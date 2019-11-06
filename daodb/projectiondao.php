5fc806081b97d926c41a89d24f490e5819e17b9f
Silvania19 <silvaniaortega17@gmail.com>
2019-11-05 23:53:51 -0300

    Create projectiondao.php

---
+++ daodb/projectiondao.php
@@ -0,0 +1,120 @@
+<?php
+namespace daodb;
+use models\Projection as projection;
+USE interfaces\Idaos as Idaos;
+use daodb\Connection as   Connection;
+
+class ProjectionDao implements Idaos
+{
+    private $connection;
+    public function __construct()
+    {
+
+    }
+    public function GetAll()
+    {
+        
+        $sql="SELECT * FROM projections";
+        try {
+            $this->connection = Connection::getInstance();
+            $listProjection = $this->connection->execute($sql);  
+           
+        } catch (\PDOException  $ex) {
+            throw $ex;
+        }
+        if (!empty($listProjection))
+        {
+            return $this->mapear($listProjection);
+          
+        }
+        else{
+            return false;
+        }
+    }
+    public function Add($objeto)
+    {
+        // Guardo como string la consulta sql utilizando como values, marcadores de parámetros con nombre (:name) o signos de interrogación (?)
+        // por los cuales los valores reales serán sustituidos cuando la sentencia sea ejecutada
+        $sql="INSERT INTO projections (date, hour, idCine, idMovie) VALUES (:date, :hour, :idCine,:idMovie)";
+      
+        $parameters["date"]=$objeto->getDate();
+        $parameters['hour']=$objeto->getHour();
+        $parameters["idCine"]=$objeto->getIdCine();
+        $parameters["idMovie"]=$objeto->getIdMovie();
+        try {
+            $this->connection= Connection::getInstance();
+            return $this->connection->executeNonQuery($sql, $parameters);
+        } catch (\PDOException $ex) {
+            throw $ex;
+        }
+    }
+   /**
+    * convertivos lo que traemos de la base de datos que es un arreglo donde 
+    *cada posicion lo que tiene es un arreglo con los datos, a un arreglo de objeto del tipo al que pertece.
+   **/
+   private function mapear($arreglo)
+   {
+       $arreglo=is_array($arreglo)?$arreglo:[];
+       $arregloObjetos=array_map(function($pos)
+       {
+        $newProjection =new projection($pos['date'],$pos['hour'], $pos['idCine'], $pos['idMovie']);
+        $newProjection->setIdProjection($pos['idProjection']);
+        return $newProjection;
+       }, $arreglo);
+       return count($arregloObjetos)>1? $arregloObjetos: $arregloObjetos['0'];
+   }
+    public function Delete($idProjection)
+    {
+        $sql = "DELETE FROM projections WHERE idProjection = :idProjection";
+        $parameters['idProjection'] = $idProjection;
+
+        try
+        {
+            $this->connection = Connection::getInstance();
+            return $this->connection->ExecuteNonQuery($sql, $parameters);
+        }
+        catch(PDOException $e)
+        {
+            echo $e;
+        }
+    }
+    public function Update($objeto, $buscador)
+    {
+        $sql="UPDATE projections SET date=:date, hour=:hour, idCine=:idCine, idMovie=:idMovie WHERE email = '$buscador';";
+        $parameters['hour']=$objeto->getHour();
+        $parameters["date"]=$objeto->getDate();
+        $parameters["idCine"]=$objeto->getIdCine();
+        $parameters['idMovie']=$objeto->getIdMovie();
+        try
+        {
+            $this->connection = Connection::getInstance();
+            return $this->connection->ExecuteNonQuery($sql, $parameters);
+        }
+        catch(PDOException $e)
+        {
+            echo $e;
+        }
+    
+    }
+    public function Search($objeto)
+    {
+      $sql="SELECT * FROM projections where idProjection=:idProjection";   
+      $parameters['idProjection']=$objeto;
+      try {
+          $this->connection = Connection:: getInstance();
+          $resul=$this->connection->execute($sql, $parameters);
+      } catch (\PDOException $th) {
+          throw $th;
+      }
+      if(!empty ($resul))
+      {
+          return $this->mapear($resul);
+      }
+      else
+      {
+          return  false;
+      }
+    }
+   
+}
+?>
