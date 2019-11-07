116d51ff3fffc743ac05867e2fa45db4dcb9d45b
Silvania19 <silvaniaortega17@gmail.com>
2019-11-07 13:59:50 -0300

    Update projectioncontrollers.php

--- controllers/projectioncontrollers.php
+++ controllers/projectioncontrollers.php
@@ -4,17 +4,19 @@ use models\Projection as projection;
 use daodb\ProjectionDao as projectionD;
 use daosjson\MovieDao as movieD;
 use daosjson\GenresDao as genreD;
+use daodb\CineDao as cineD;
 class ProjectionControllers
 {
    private $listMovie;
    private $listGenre;
    private $listProjection;
- 
+   private $listCine;
   public function __construct()
   {
      $this->listMovie=new movieD();
      $this->listGenre=new genreD();
      $this->listProjection=new projectionD();
+     $this->listCine=new cineD();
   }
  public function addparte1()
  {
@@ -40,15 +42,23 @@ class ProjectionControllers
   $idMovie=$array['0'];
   $idCine=$array['1'];
   
-  $projection= new projection($date, $hour, $idCine, $idMovie);
-  $this->listProjection->Add($projection);
- 
+   $projection= new projection($date, $hour, $idCine, $idMovie);
+   $this->listProjection->Add($projection);
+   $cines=$this->listCine->GetAll();
+    $cartelera=$this->listProjection->GetAll();
+    $movies=$this->listMovie->getNowMovie();
+    echo" <script>alert('added projection');</script>" ;
+    include(VIEWS_PATH."carteleraviews.php");
  }
  public function delete()
  {
    $idProjection=$_POST["idProjection"];
    $this->listProjection->Delete($idProjection);
-
+   $cines=$this->listCine->GetAll();
+   $cartelera=$this->listProjection->GetAll();
+   $movies=$this->listMovie->getNowMovie();
+   echo" <script>alert('deleted projection');</script>" ;
+   include(VIEWS_PATH."carteleraviews.php");
  }
 
 }
