8c248b8d0e10357fc0ffb90554c6bc21ac73f445
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-08 18:04:18 -0300

    Update projectioncontrollers.php

--- controllers/projectioncontrollers.php
+++ controllers/projectioncontrollers.php
@@ -22,32 +22,34 @@ class ProjectionControllers
  {
    $idCine=$_POST['idCine'];
    $control=1;
-   $listMovie2=$this->listMovie->getNowMovie();
+   $listProjection2=$this->listProjection->GetAll();
+   $listMovie2=$this->listMovie->GetAll();
    $listGenres2=$this->listGenre->GetAll();
    include(VIEWS_PATH."carteleraviews.php");
  }
  public function addparte2()
  {
-  $datos=$_POST['idMovie'];
+  $datos=$_POST['datos'];
   $control2=1;
 
   include(VIEWS_PATH."carteleraviews.php");
  }
- public function add()
+ public function addProjection()
  {
   $datos=$_POST['datos'];
+
   $date=$_POST['date'];
   $hour=$_POST['hour'];
   $array=explode("-", $datos);
   $idMovie=$array['0'];
   $idCine=$array['1'];
-  
+
    $projection= new projection($date, $hour, $idCine, $idMovie);
    $this->listProjection->Add($projection);
    $cines=$this->listCine->GetAll();
     $cartelera=$this->listProjection->GetAll();
-    $movies=$this->listMovie->getNowMovie();
-    echo" <script>alert('added projection');</script>" ;
+    $movies=$this->listMovie->GetAll();
+  echo" <script>alert('added projection');</script>" ;
     include(VIEWS_PATH."carteleraviews.php");
  }
  public function delete()
@@ -56,10 +58,46 @@ class ProjectionControllers
    $this->listProjection->Delete($idProjection);
    $cines=$this->listCine->GetAll();
    $cartelera=$this->listProjection->GetAll();
-   $movies=$this->listMovie->getNowMovie();
+   $movies=$this->listMovie->GetAll();
    echo" <script>alert('deleted projection');</script>" ;
    include(VIEWS_PATH."carteleraviews.php");
  }
+ public function exist($idMovie, $idCine)
+  {
+   
+      $projections= $this->listProjection->SearchXCine($idCine);
+      $retorno=false;
+      if($projections !=false){
+      if(is_array($projections))
+      {
+        foreach ($projections as $value)
+         {
+           
+         
+            if($value->getIdMovie()==$idMovie) 
+             {
+               
+              $retorno= true;
+             }
+
+        }
+      
+      }
+      if(is_object($projections))
+      {
+        if($projections->getIdMovie()==$idMovie)
+        {
+          $retorno= true;
+        }
+       
+      }
+    }
+    else 
+    {
+      $retorno= false;
+    }
+    return $retorno;
+  }
 
 }
 
