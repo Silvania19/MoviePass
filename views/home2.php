fa6a2472d65e88e42cfcba580fc40cf61d6a5744
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-11 18:23:37 -0300

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -1,35 +1,42 @@
-<?php include(VIEWS_PATH."header.php"); ?>
+<?php include(VIEWS_PATH."header.php"); 
+      include(VIEWS_PATH."nav.php");
+?>
+
 
    <nav class="navbar navbar-expand-sm bg-danger">
           <a href=""class="navbar-brand siForm"><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
       <ul class="navbar-nav ml-auto">
 
- 
-           
+
+<ion-header>
+    <ion-toolbar>
+          <ion-buttons slot="start">
+                 <ion-back-button > </ion-back-button> 
+           </ion-buttons>
+            <ion-title>Volver </ion-title>
+     </ion-toolbar>
+</ion-header>
+
+
                 <li class ="nav-item">
                   <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link " >PELICULAS</a>
                 </li>
                 <li class ="nav-item">
                   <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link " >CINES</a>
                 </li>
-               
+               <li>
                 <a href="<?php echo FRONT_ROOT;?>/views/cartelera" class="nav-link">CARTELERA</a>
                 </li>
 
                 <li class ="nav-item dropdown float-right">
                <button type="button" class="btn btn-primary" data-toggle="dropdown">USUARIO</button>
-               <div class="dropdown-menu">
-               <a href="" class="dropdown-item"><?php echo $_SESSION['user']->getName();?></a>
-               <a href="<?php echo FRONT_ROOT;?>/views/user" class="dropdown-item">Ver Perfil</a>
-               <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="dropdown-item">Salir</a>
-               </div>
-                </li>
+              </li>
                
-            </ul>
+     </ul>
          
-        </div>
-    </li>
-    </nav>
+       
+    
+  </nav>
     
   
     <?php include(VIEWS_PATH."footer.php");?>
