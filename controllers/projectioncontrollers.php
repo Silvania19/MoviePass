e7d2fe379ff03efdb7793059391ba0a7f64f2533
Silvania19 <silvaniaortega17@gmail.com>
2019-11-11 16:12:13 -0300

    Update projectioncontrollers.php

--- controllers/projectioncontrollers.php
+++ controllers/projectioncontrollers.php
@@ -5,56 +5,75 @@ use daodb\ProjectionDao as projectionD;
 use daosjson\MovieDao as movieD;
 use daosjson\GenresDao as genreD;
 use daodb\CineDao as cineD;
+use daodb\CinemaDao as cinemaD;
 class ProjectionControllers
 {
    private $listMovie;
    private $listGenre;
    private $listProjection;
    private $listCine;
+   private $listCinema;
   public function __construct()
   {
      $this->listMovie=new movieD();
      $this->listGenre=new genreD();
      $this->listProjection=new projectionD();
      $this->listCine=new cineD();
+     $this->listCinema=new cinemaD();
   }
- public function addparte1()
+ public function addparte1($idCine=null)
  {
-   $idCine=$_POST['idCine'];
+  
    $control=1;
    $listProjection2=$this->listProjection->GetAll();
    $listMovie2=$this->listMovie->GetAll();
    $listGenres2=$this->listGenre->GetAll();
    include(VIEWS_PATH."carteleraviews.php");
  }
- public function addparte2()
+ public function addparte2($datos=null)
  {
-  $datos=$_POST['datos'];
+  $array=explode("-", $datos);
   $control2=1;
-
+  $listCinemas2=$this->listCinema->SearchIdCine($array['1']);
   include(VIEWS_PATH."carteleraviews.php");
  }
- public function addProjection()
+ public function addProjection($datos=null, $datos=null, $hour=null)
  {
-  $datos=$_POST['datos'];
-
-  $date=$_POST['date'];
-  $hour=$_POST['hour'];
+  
   $array=explode("-", $datos);
   $idMovie=$array['0'];
   $idCine=$array['1'];
+  $veri=0;
+  $cartelera=$this->listProjection->GetAll();
+  $cines=$this->listCine->GetAll();
+  $movies=$this->listMovie->GetAll();
+  foreach($cartelera as $projection)
+  {
+    if($projection->getDate()==$date && $projection->getIdMovie()==$idMovie){
 
-   $projection= new projection($date, $hour, $idCine, $idMovie);
-   $this->listProjection->Add($projection);
-   $cines=$this->listCine->GetAll();
-    $cartelera=$this->listProjection->GetAll();
-    $movies=$this->listMovie->GetAll();
-  echo" <script>alert('added projection');</script>" ;
+      $veri=1;
+  }
+  
+      
+  }
+  if($veri==1)
+  {
+    echo" <script>alert('no se puede agregar en este fecha. Verifique que la fecha ingresada no sea igual a la de otro de cine');</script>" ;
     include(VIEWS_PATH."carteleraviews.php");
+  }
+  else
+  {
+      $projection= new projection($date, $hour, $idCine, $idMovie);
+      $this->listProjection->Add($projection);
+       echo" <script>alert('added projection');</script>" ;
+       include(VIEWS_PATH."carteleraviews.php");
+    
+  }
+
  }
- public function delete()
+ public function delete($idProjection=null)
  {
-   $idProjection=$_POST["idProjection"];
+  
    $this->listProjection->Delete($idProjection);
    $cines=$this->listCine->GetAll();
    $cartelera=$this->listProjection->GetAll();
