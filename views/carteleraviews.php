f10852f731bcec6ca2bbbf09df163451f5d398e2
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-11 18:23:02 -0300

    Update carteleraviews.php

--- views/carteleraviews.php
+++ views/carteleraviews.php
@@ -1,23 +1,11 @@
 <?php include(VIEWS_PATH."header.php");
+      include(VIEWS_PATH."nav.php");
 if(!isset($control)&& !isset($control2))
 {
 
 
 ?>
-<nav class="navbar navbar-expand-sm bg-danger">
-        <ul class="navbar-nav ml-auto">
-           
-             <li class ="nav-item dropdown ">
-               <button type="button" class="btn btn-primary" data-toggle="dropdown">USUARIO</button>
-               <div class="dropdown-menu">
-               <a href="" class="dropdown-item"><?php echo $_SESSION['user']->getName();?></a>
-               <a href="<?php echo FRONT_ROOT;?>/views/user" class="dropdown-item">Ver Perfil</a>
-               <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="dropdown-item">Salir</a>
-               
-             </li>
 
-        </ul>
-    </nav>
 
    <div class="col" style="border:1px solid gray;">
        
