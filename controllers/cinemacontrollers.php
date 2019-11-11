423339a5203c8029772ca449fa23ae0afc58e287
Silvania19 <silvaniaortega17@gmail.com>
2019-11-11 16:12:21 -0300

    Update cinemacontrollers.php

--- controllers/cinemacontrollers.php
+++ controllers/cinemacontrollers.php
@@ -13,26 +13,21 @@
          $this->cineList= new cineD();
      }
 
-     public function add()
+     public function add( $idCine=null, $nameCinema=null, $capacity=null)
      {
-        $idCine=$_POST['idCine'];
-        $nameCinema=$_POST['nameCinema'];
-        $capacity=$_POST['capacity'];
+       
       
         $newCinema= new cinema($idCine, $nameCinema, $capacity);
-       
-    $this->cinemaList->Add($newCinema);
-    $listCine2=$this->cineList->GetAll();
-    $listCinema2=$this->cinemaList->GetAll();
-    echo" <script>alert('added cinema');</script>" ;
-    include(VIEWS_PATH."cinemaviews.php");
+        $this->cinemaList->Add($newCinema);
+        $listCine2=$this->cineList->GetAll();
+        $listCinema2=$this->cinemaList->GetAll();
+        echo" <script>alert('added cinema');</script>" ;
+        include(VIEWS_PATH."cinemaviews.php");
     
         
      }
-     public function remove()
+     public function remove( $idCinema=null)
      {
-         $idCinema=$_POST['idCinema'];
-        
         $this->cinemaList->Delete($idCinema);
         
         $listCine2=$this->cineList->GetAll();
