90a84be5b5f9579c5a7b990d9118758f343e2342
Silvania19 <silvaniaortega17@gmail.com>
2019-11-14 14:51:17 -0300

    Update projection.php

--- models/projection.php
+++ models/projection.php
@@ -7,13 +7,17 @@ class Projection
     private $hour;
     private $idCine;
     private $idMovie;
+    private $idCinema;
+    private $duration;
 
-    public function __construct( $date, $hour, $idCine, $idMovie)
+    public function __construct( $date, $hour, $idCine, $idMovie, $idCinema, $duration)
     {
         $this->date=$date;
         $this->hour=$hour;
         $this->idCine=$idCine;
         $this->idMovie=$idMovie;
+        $this->idCinema=$idCinema;
+        $this->duracion=$duration;
     }
     public function setIdProjection($idProjection)
     {
@@ -36,6 +40,14 @@ class Projection
         $this->idMovie=$idMovie;
 
     }
+    public function setIdCinema($idCinema)
+    {
+        $this->idCinema=$idCinema;
+    }
+    public function setDuration($duration)
+    {
+        $this->duracion=$duration;
+    }
     public function getIdProjection()
     {
         return $this->idProjection;
@@ -55,6 +67,14 @@ class Projection
     public function getIdMovie()
     {
         return $this->idMovie;
+    }
+    public function getIdCinema()
+    {
+        return $this->idCinema;
+    }
+    public function getDuration()
+    {
+        return $this->duration;
     }
 }
 
