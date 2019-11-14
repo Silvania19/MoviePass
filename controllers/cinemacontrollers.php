b9b48ff0697e3bd3d62c42fc1e5ac283be8955dc
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-14 15:35:23 -0300

    Update cinemacontrollers.php

--- controllers/cinemacontrollers.php
+++ controllers/cinemacontrollers.php
@@ -29,14 +29,22 @@
     
         
      }
-     public function remove($idCinema=null)
+     public function remove($idCinema=null,$verificacion=null)
      {
+        if($verificacion=='si')
+        {
         $this->cinemaList->Delete($idCinema);
         $user=$_SESSION['user'];
         $listCines=$this->cineList->GetAll();  
         echo" <script>alert('deleted cinema');</script>" ;
         include(VIEWS_PATH."cineviews.php");
+         }
+         else if($verificacion=='no')
+         {
+            include(VIEWS_PATH."cineviews.php");
+         }
+       
  
      }
  }
-?>
+?>j
