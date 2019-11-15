f7c1c4359b5b4ddd57d89368d52fb90d6d43c064
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-15 17:37:34 -0300

    Create navusers.php

---
+++ views/navusers.php
@@ -0,0 +1,16 @@
+<?php include(VIEWS_PATH."header.php"); ?>
+    <nav class="navbar navbar-expand-sm bg-danger">
+    <a href=""class="navbar-brand siForm"><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
+        <ul class="navbar-nav ml-auto">
+           
+             <li class ="nav-item dropdown ">
+             
+
+
+               <button type="button" class="btn btn-primary" data-toggle="dropdown"><a href="javascript:history.back()"><img src="<?php echo FRONT_ROOT;?>/front/img/botonatras.jpg" height="33" width="100" alt="Botón" </a> </button>
+              
+             </li>
+
+        </ul>
+    </nav>
+<?php include(VIEWS_PATH."footer.php"); 
