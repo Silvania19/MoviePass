e15b09ef2f0127a4a2ef5550eaf83b6a91ede8c1
Silvania19 <silvaniaortega17@gmail.com>
2019-11-14 21:18:48 -0300

    Update cineviews.php

--- views/cineviews.php
+++ views/cineviews.php
@@ -1,4 +1,5 @@
-<?php include(VIEWS_PATH."header.php");?>
+<?php include(VIEWS_PATH."header.php");
+ include(VIEWS_PATH."nav.php");?>
 <?php
     
     if($user->getIdRol()==2)
@@ -95,25 +96,10 @@
  } 
  if($user->getIdRol()==1)
  {
+   include(VIEWS_PATH."nav.php");
 ?>
 
-<nav class="navbar navbar-expand-sm bg-danger"> 
-<a class="navbar-brand"><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
-      <ul class="navbar-nav ml-auto">
-     
-        <li class="nav-item"><a href="" class="nav-link"></a>
 
-                <li class ="nav-item dropdown ">
-               <button type="button" class="btn btn-primary" data-toggle="dropdown">USUARIO</button>
-               <div class="dropdown-menu">
-               <a href="" class="dropdown-item"><?php echo $_SESSION['user']->getName();?></a>
-               <a href="<?php echo FRONT_ROOT;?>/views/user" class="dropdown-item">Ver Perfil</a>
-               <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="dropdown-item">Salir</a>
-               </div>
-                </li>
-
-      </ul>
-</nav>
 <div class="col" style="border:1px solid gray;">
        
        <table class="  table-borderer table-hover ">
