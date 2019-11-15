807c0b29518971a493266ba29bb4f4a4a3de7b22
Silvania19 <silvaniaortega17@gmail.com>
2019-11-15 17:43:06 -0300

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -1,39 +1,74 @@
 <?php include(VIEWS_PATH."header.php"); 
-      
+   if($user->getIdRol()==2)  
+   {
 ?>
 
 
-   <nav class="navbar navbar-expand-sm bg-danger"> 
-      <a href=""class="navbar-brand "><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
+      <nav class="navbar navbar-expand-sm bg-danger"> 
+        <a href=""class="navbar-brand "><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
 
-             <ul class="navbar-nav ml-auto">
-          
-           
-                <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link " >PELICULAS</a>
-                </li>
-                <li class ="nav-item">
-                  <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link " >CINES</a>
-                </li>
-               <li>
-                <a href="<?php echo FRONT_ROOT;?>/views/cartelera" class="nav-link">CARTELERA</a>
-                </li>
-               <li class ="nav-item  ">
-                <div class="dropdown">
-                   <button type="button" class="btn btn-primary dropdown-toggle"  data-toggle="dropdown">USUARIO</button>
-                    <div class="dropdown-menu">
-                     <a href="" class="dropdown-item"><?php echo $_SESSION['user']->getName();?></a>
-                     <a href="<?php echo FRONT_ROOT;?>/views/user" class="dropdown-item">Ver Perfil</a>
-                     <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="dropdown-item">Salir</a>
-                    </div>
-               </div>
-             </li>
-               
-     </ul>
-         
-       
-    
-  </nav>
- 
+        <ul class="navbar-nav ml-auto">
+              
+              
+          <li class ="nav-item">
+            <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link " >PELICULAS</a>
+          </li>
+          <li class ="nav-item">
+            <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link " >CINES</a>
+          </li>
+          <li>
+            <a href="<?php echo FRONT_ROOT;?>/views/cartelera" class="nav-link">CARTELERA</a>
+          </li>
+          <li class ="nav-item  ">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle"  data-toggle="dropdown">USUARIO</button>
+                <div class="dropdown-menu">
+                        <a href="" class="dropdown-item"><?php echo $_SESSION['user']->getName();?></a>
+                        <a href="<?php echo FRONT_ROOT;?>/views/user" class="dropdown-item">Ver Perfil</a>
+                        <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="dropdown-item">Salir</a>
+                </div>
+            </div>
+          </li>
+                  
+        </ul>
+       </nav>
+    <?php 
+     } 
+     
+     if($user->getIdRol()==1)
+     {
+    ?>
+       <nav class="navbar navbar-expand-sm bg-danger"> 
+        <a href=""class="navbar-brand "><img src="<?php echo FRONT_ROOT;?>/front/img/dog.jpeg" style="width: 70px;"></a>
+
+        <ul class="navbar-nav ml-auto">
+              
+              
+          <li class ="nav-item">
+            <a href="<?php echo FRONT_ROOT;?>/movie/seeListMovie" class="nav-link " >PELICULAS</a>
+          </li>
+          <li class ="nav-item">
+            <a href="<?php echo FRONT_ROOT;?>/views/cine" class="nav-link " >CINES</a>
+          </li>
+          <li>
+            <a href="<?php echo FRONT_ROOT;?>/views/cartelerauser" class="nav-link">CARTELERA</a>
+          </li>
+          <li class ="nav-item  ">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle"  data-toggle="dropdown">USUARIO</button>
+                <div class="dropdown-menu">
+                        <a href="" class="dropdown-item"><?php echo $_SESSION['user']->getName();?></a>
+                        <a href="<?php echo FRONT_ROOT;?>/views/user" class="dropdown-item">Ver Perfil</a>
+                        <a href="<?php echo FRONT_ROOT;?>/views/deleteSession" class="dropdown-item">Salir</a>
+                </div>
+            </div>
+          </li>
+                  
+        </ul>
+       </nav>
+
+    <?php
+     }
+    ?>
   
-    <?php include(VIEWS_PATH."footer.php");?>
+<?php include(VIEWS_PATH."footer.php");?>
