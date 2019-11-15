04393cbf55ab6f2c0a412da5744a7c6f5d012237
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-14 21:23:35 -0300

    Update cinecontrollers.php

--- controllers/cinecontrollers.php
+++ controllers/cinecontrollers.php
@@ -3,19 +3,25 @@ namespace controllers;
 use models\Cine as cine;
 use daodb\CineDao as cineD;
 use daodb\CinemaDao as cinemaD;
+use daodb\Projectiondao as projectionD;
+use controllers\UserControllers as userC;
 class CineControllers
 {
     private $cineRepo;//esta variable, sera aquella aque le iguale lo que traiga de la base de datos. Cuando la tenga
     private $cinemaList;
+    private $userControllers;
+    private $projectionList;
     public function __construct()
     {
         $this->cineRepo= new cineD();// sera reemplazado por una instancia de la clases donde tenga  la base correspondiente
         $this->cinemaList= new cinemaD();
+        $this->userControllers= new userC();
+        $this->projectionList= new projectionD();
      }
     
     public function add($name=null, $address=null)
     { 
-       $user=$_SESSION['user'];
+       $user=$this->userControllers->checkSession();
       $cine=new cine($name,  $user->getIdUser(), $address);
       $this->cineRepo->Add($cine);
       $listCines=$this->cineRepo->GetAll();
@@ -26,8 +32,8 @@ class CineControllers
 
     public function update($name=null, $address=null, $idCine=null)
     {
-      $user=$_SESSION['user'];
-      $cine=new cine($name, $user->getIdUser(),  $address);
+      $user=$this->userControllers->checkSession();
+      $cine=new cine($name, $user->getIdUser(), $address);
       $this->cineRepo->Update($cine, $idCine);
       $listCines=$this->cineRepo->GetAll();
       echo" <script>alert('updated cine');</script>" ;
@@ -35,20 +41,39 @@ class CineControllers
       
     }
 
-    public function remove($idCine=null)
+    public function remove($verificacion=null)
     {
-      $listCinema=$this->cinemaList->GetAll();
-      foreach($listCinema as $cinema)
-     {
-         if($cinema->getIdCine()==$idCine)
-         {
-             $this->cinemaList->Delete($cinema->getIdCinema());
-         }
-     }
-     $this->cineRepo->Delete($idCine);
-     $listCines=$this->cineRepo->GetAll();
-     echo" <script>alert('deleted cine');</script>" ;
-     include(VIEWS_PATH."cineviews.php");
-     
+      $user=$this->userControllers->checkSession();
+      
+      if($verificacion=='no')
+      {
+        $listCines=$this->cineRepo->GetAll();
+        include(VIEWS_PATH."cineviews.php");
+      }
+      else 
+      {
+        $idCine=$verificacion;
+        $listCinema=$this->cinemaList->GetAll();
+        $projectionL=$this->projectionList->GetAll();
+        foreach($listCinema as $cinema)
+        {
+            if($cinema->getIdCine()==$idCine)
+            {
+                $this->cinemaList->Delete($cinema->getIdCinema());
+            }
+        }
+        foreach($projectionL as $projection)
+        {
+            if($projection->getIdCine()==$idCine)
+            {
+                $this->projectionList->Delete($projection->getIdProjection());
+            }
+        }
+        $this->cineRepo->Delete($idCine);
+        $listCines=$this->cineRepo->GetAll();
+        echo" <script>alert('deleted cine');</script>" ;
+        include(VIEWS_PATH."cineviews.php");
+      }
+        
     }
 }
