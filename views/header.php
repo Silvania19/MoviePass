34c37878ced0b5552a2767d7c0fd68fa495e9636
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-15 17:38:12 -0300

    Update header.php

--- views/header.php
+++ views/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
- 
+   
+    <link rel="stylesheet" href="<?php echo FRONT_ROOT;?>/front/img/cine.jpg">
     <link rel="stylesheet" href="<?php echo FRONT_ROOT;?>/front/css/bootstrap.css">
     <link rel="stylesheet" href="<?php echo FRONT_ROOT;?>/front/styles/forms.css">
     
