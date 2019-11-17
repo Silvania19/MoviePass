a1f4fb138484807db449e23168035af39515e958
Florencia18Juarez <florencia.juarez.159@gmail.com>
2019-11-16 21:00:25 -0300

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -95,10 +95,10 @@
       ?>
             <img src="https://image.tmdb.org/t/p/w500/<?php echo $movie->getPoster_path();?>" alt= "<?php $movie->getTitle();?> " class="rounded"><br>
             
-            <form action="<?php echo FRONT_ROOT; ?>/projection/addparte2" method="post"><!--esta direccion esta mal ahi que mandarlo a un metodo de la cartelera que le muestre, solo
+            <form action="<?php echo FRONT_ROOT; ?>/projectionuser/moviexCines" method="post"><!--esta direccion esta mal ahi que mandarlo a un metodo de la cartelera que le muestre, solo
             las funciones de la pelicula que se esta leyendo-->
                  <h3>Title: <input type="submit" value=" <?php echo $movie->getTitle(); ?> ">
-                 <input type="checkbox" name="datos" id="" value="<?php echo $movie->getIdMovie()?>"><!-- creo que esta bien porque con el idDe la pelicula podriamos mostrar todas las funciones
+                 <input type="checkbox" name="datos" id="" value="<?php  echo $movie->getIdMovie();?>"><!-- creo que esta bien porque con el idDe la pelicula podriamos mostrar todas las funciones
                  de esa pelicula en el futuro-->
                  </h3>
             </form>
@@ -143,7 +143,7 @@
         ?>
                  <img src="https://image.tmdb.org/t/p/w500/<?php echo $movie->getPoster_path();?>" alt= "<?php $movie->getTitle();?> " class="rounded"><br>
             
-            <form action="<?php echo FRONT_ROOT; ?>/projection/addparte2" method="post"><!--esta direccion esta mal ahi que mandarlo a un metodo de la cartelera que le muestre, solo
+            <form action="<?php echo FRONT_ROOT; ?>/projectionuser/moviesxCines" method="post"><!--esta direccion esta mal ahi que mandarlo a un metodo de la cartelera que le muestre, solo
             las funciones de la pelicula que se esta leyendo-->
                  <h3>Title: <input type="submit" value=" <?php echo $movie->getTitle(); ?> ">
                  <input type="checkbox" name="datos" id="" value="<?php echo $movie->getIdMovie()?>"><!-- creo que esta bien porque con el idDe la pelicula podriamos mostrar todas las funciones
