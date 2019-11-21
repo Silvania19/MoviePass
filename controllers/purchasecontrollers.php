b441e18329c02b49e3356728684af2bdc3dbffa0
Silvania19 <silvaniaortega17@gmail.com>
2019-11-21 02:39:42 -0300

    h

--- controllers/purchasecontrollers.php
+++ controllers/purchasecontrollers.php
@@ -127,9 +127,7 @@
             }
             
         }
-         
-
-        public function knowAmount($idPurchase=null)
+         public function knowAmount($idPurchase=null)
         {
             
            $purchasesWithPay=array();
